7a7312896abef31b59726d80e64db5168a5e70cd
IwamotoKosuke <iwamoto.dengana@gmail.com>
2020-08-14 20:45:07 +0900

    指摘：大文字と小文字の区別をする

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i, message: 'Include both letters and numbers' }
-    # 指摘内容がわかりませんでした。githubのコメント内容をご確認願います
-    validates :nickname, presence: true
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}s+\z/i, message: 'Include both letters and numbers' }
+    validates :nickname, presence: true, uniqueness: { case_sensitive: true }
     validates :email, presence: true
     validates :first_name_zenkaku, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
     validates :last_name_zenkaku, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
