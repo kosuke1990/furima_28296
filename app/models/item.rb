e3afec17840fcae7fd02cbdb5fd529b3f6270204
IwamotoKosuke <iwamoto.dengana@gmail.com>
2020-08-18 15:00:16 +0900

    単体テストコード実装（途中まで）

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
 
   with_options presence: true do
-    validates :price, presence: true
+    validates :name, presence: true
+    validates :explanation, presence: true
+    validates :price, presence: true, numericality: {less_than: 10000000, greater_than: 300}
     #ジャンルの選択が「--」の時は保存できないようにする
     validates :category_id, :status_id, :who_cover_fee_id, :where_delivery_from_id, :how_long_id, numericality: { other_than: 1 } 
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,31 +13,59 @@ RSpec.describe Item, type: :model do
       end
       it '新規商品投稿に必須事項を入力したら新規商品を投稿できること' do
       end
+      it '新規商品投稿のpriceが300円で新規商品を投稿できること' do
+      end
+      it '新規商品投稿のpriceが9999999円で新規商品を投稿できること' do
+      end
     end
     context '新規投稿商品がうまくいかない時' do
       it '商品名がない時にエラーメッセージが出力されること' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it '商品の説明がない時にエラーメッセージが出力されること' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
       it 'カテゴリーの情報がない時にエラーメッセージが出力されること' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態についての情報がない時にエラーメッセージが出力されること' do
+        @item.status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it '配送料の負担についての情報がない時にエラーメッセージが出力されること' do
+        @item.who_cover_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Who cover fee can't be blank")
       end
       it '発送元の地域についての情報がない時にエラーメッセージが出力されること' do
+        @item.where_delivery_from_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Where delivery from can't be blank")
       end
       it '発送までの日数についての情報がない時にエラーメッセージが出力されること' do
+        @item.how_long_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("How long can't be blank")
       end
       it '価格についての情報がない時にエラーメッセージが出力されること' do
         @item.price = ''
         @item.valid?
-        
-        binding.pry
-        
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格情報がprice<300||price>9999999の時にエラーが出力されること' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 300")
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
     end
   end
